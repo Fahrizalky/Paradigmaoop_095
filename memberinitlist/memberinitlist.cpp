1b67a24c70e6ed107279c92bb6057120a80b6c6d
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-24 14:17:16 +0700

    membuat kelas mahasiswa yang bersifat public atau memberi initialization list

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -8,3 +8,8 @@ private:
 	string nama;
 	float nilai;
 
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Memberi initialization list
+		//Definisi
+	}
+
