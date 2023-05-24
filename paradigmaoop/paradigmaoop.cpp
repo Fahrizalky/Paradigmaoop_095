d432751424f3a4eaf4bcd24a2259b765bb9f48b4
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-05-24 11:12:19 +0700

    membuat class orang

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+	int jumlaha(int a, int b) {
+		return a + b;
+	}
+};
+
+
 
